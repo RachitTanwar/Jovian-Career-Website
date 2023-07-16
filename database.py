d53f3a09a32170386dc68342f968a1c29737f548
RachitTanwar <tanwarrachit6@gmail.com>
2023-07-16 10:47:33 +0530

    JobPage Added

--- database.py
+++ database.py
@@ -17,3 +17,15 @@ def load_jobs_from_db():
         data = row._mapping  
         jobs.append(data)
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return (dict(rows[0]))
